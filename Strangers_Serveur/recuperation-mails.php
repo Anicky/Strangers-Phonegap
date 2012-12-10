034360d6e29eb421a83f0f343a70f8d1465339be
Jeremie JALOUZET <jeremie.jalouzet@utt.fr>
2012-12-10 21:56:58 +0100

    Ajout de quelques tests pour éviter des erreurs

--- Strangers_Serveur/recuperation-mails.php
+++ Strangers_Serveur/recuperation-mails.php
@@ -8,45 +8,49 @@ if ((isset($_POST['num'])) && (isset($_POST['serv'])) && (isset($_POST['port']))
         $port = $_POST['port'];
         $username = $_POST['user'];
         $password = $_POST['pass'];
-        $folder = $_POST['folder'];
+        $folder = "INBOX";
+        if (isset($_POST['folder'])) {
+            $folder = $_POST['folder'];
+        }
 
         /* Récupération des mails */
         $hostname = '{' . $serveur . ':' . $port . '/imap/ssl}' . $folder;
 
         $inbox = imap_open($hostname, $username, $password) or die('Problème de connexion : ' . imap_last_error());
 
-        $emails = imap_search($inbox, 'ALL');
+        if ($inbox) {
+            $emails = imap_search($inbox, 'ALL');
 
-        /* if emails are returned, cycle through each... */
-        if ($emails) {
+            /* if emails are returned, cycle through each... */
+            if ($emails) {
 
-            /* begin output var */
-            $output = '';
+                /* begin output var */
+                $output = '';
 
-            /* put the newest emails on top */
-            rsort($emails);
+                /* put the newest emails on top */
+                rsort($emails);
 
-            /* for every email... */
-            foreach ($emails as $email_number) {
+                /* for every email... */
+                foreach ($emails as $email_number) {
 
-                /* get information specific to this email */
-                $overview = imap_fetch_overview($inbox, $email_number, 0);
-                $message = imap_fetchbody($inbox, $email_number, 2);
+                    /* get information specific to this email */
+                    $overview = imap_fetch_overview($inbox, $email_number, 0);
+                    $message = imap_fetchbody($inbox, $email_number, 2);
 
-                /* output the email header information */
-                $output.= '<div class="toggler ' . ($overview[0]->seen ? 'read' : 'unread') . '">';
-                $output.= '<span class="subject">' . $overview[0]->subject . '</span> ';
-                $output.= '<span class="from">' . $overview[0]->from . '</span>';
-                $output.= '<span class="date">on ' . $overview[0]->date . '</span>';
-                $output.= '</div>';
+                    /* output the email header information */
+                    $output.= '<div class="toggler ' . ($overview[0]->seen ? 'read' : 'unread') . '">';
+                    $output.= '<span class="subject">' . $overview[0]->subject . '</span> ';
+                    $output.= '<span class="from">' . $overview[0]->from . '</span>';
+                    $output.= '<span class="date">on ' . $overview[0]->date . '</span>';
+                    $output.= '</div>';
 
-                /* output the email body */
-                $output.= '<div class="body">' . $message . '</div>';
+                    /* output the email body */
+                    $output.= '<div class="body">' . $message . '</div>';
+                }
+
+                echo $output;
             }
-
-            echo $output;
         }
-
         /* close the connection */
         imap_close($inbox);
     }
